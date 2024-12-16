bd7873a574a6266d271cc9b5665370c20bf6a1dd
ghostRiderDev <olvadis2004@gmail.com>
2024-12-15 22:49:28 -0500

    tested domain model

--- src/user/domain/model/User.ts
+++ src/user/domain/model/User.ts
@@ -1,15 +1,32 @@
+import { isEmpty } from 'class-validator';
 import { UserRole } from '../enum/UserRole';
+import { UserInvalid } from '../error/UserInvalid';
+import { Constant } from '../enum/Constant';
 
 export class User {
+  readonly id: string;
+  readonly username: string;
+  readonly email: string;
+  readonly password: string;
+  readonly role: UserRole;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+
   constructor(
-    readonly id: string,
-    readonly username: string,
-    readonly email: string,
-    readonly password: string,
-    readonly role: UserRole,
-    readonly createdAt: Date,
-    readonly updatedAt: Date,
+    id: string,
+    username: string,
+    email: string,
+    password: string,
+    createdAt: Date,
+    role: UserRole,
+    updatedAt: Date,
   ) {
+    this.validNonEmpty(username);
+    this.validNonEmpty(email);
+    this.validNonEmpty(password);
+    this.validNonEmpty(role);
+    this.validRole(role);
+
     this.id = id;
     this.username = username;
     this.email = email;
@@ -17,5 +34,17 @@ export class User {
     this.createdAt = createdAt;
     this.role = role;
     this.updatedAt = updatedAt;
+  }
+
+  validNonEmpty(value: string) {
+    if (isEmpty(value)) {
+      throw new UserInvalid(Constant.INVALID_USER);
+    }
+  }
+
+  validRole(role: UserRole) {
+    if (!Object.values(UserRole).includes(role)) {
+      throw new UserInvalid(Constant.INVALID_USER);
+    }
   }
 }
