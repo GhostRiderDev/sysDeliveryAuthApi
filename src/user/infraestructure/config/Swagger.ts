2e036eb08f15d362fd03cd7dee545f6b849a627d
ghostRiderDev <olvadis2004@gmail.com>
2024-12-23 21:36:03 -0500

    feat: secure swagger doc require jwt token

--- src/user/infraestructure/config/Swagger.ts
+++ src/user/infraestructure/config/Swagger.ts
@@ -6,6 +6,11 @@ export function configureSwagger(app: INestApplication) {
     .setTitle('Delivery API')
     .setDescription('The delivery API description')
     .setVersion('1.0')
+    .addSecurity('bearer', {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    })
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
